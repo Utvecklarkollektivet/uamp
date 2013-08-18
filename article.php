870ad98cf524f58f66f22da3e39df5840cc1fcee
Christoffer Artmann <Artgaard@gmail.com>
2013-08-18 14:31:02 +0200

    Users can approve and disaprove articles

--- article.php
+++ article.php
@@ -21,4 +21,31 @@ class UAMP_Article {
 		$count = (int) $result[0]->count;
 		return ($count > 2);
 	}
+
+	public function getApproval($userID) {
+		global $wpdb;
+		$wpdb->uamp_approves = "{$wpdb->prefix}uamp_approves";
+		$sql = $wpdb->prepare("SELECT * FROM {$wpdb->uamp_approves} WHERE post_id=%d and user_id=%d LIMIT 1", array($this->id, $userID));
+		$result = $wpdb->get_results($sql);
+		if(count($result)) {
+			return $result[0];
+		}
+		return false;
+	}	
+
+	public function approve($userID, $desc) {
+		if(!$this->getApproval($userID)) {
+			global $wpdb;
+			$wpdb->uamp_approves = "{$wpdb->prefix}uamp_approves";
+			$sql = $wpdb->prepare("INSERT INTO {$wpdb->uamp_approves} (post_id, user_id, description) VALUES(%d, %d, %s)", array($this->id, $userID, $desc));
+			$wpdb->query($sql);
+		}
+	}
+
+	public function disapprove($userID) {
+		global $wpdb;
+		$wpdb->uamp_approves = "{$wpdb->prefix}uamp_approves";
+		$sql = $wpdb->prepare("DELETE FROM {$wpdb->uamp_approves} WHERE post_id=%d AND user_id=%d", array($this->id, $userID, $desc));
+		$wpdb->query($sql);	
+	}
 }
--- single-uamp_article.php
+++ single-uamp_article.php
@@ -10,7 +10,8 @@ get_header();
 	<div id="content" role="main">
 	<?php
 		$loop = new WP_Query(array("post_type" => "uamp_article"));
-
+		if(is_user_logged_in())
+			$userID = get_current_user_id();
 		while($loop->have_posts() ): $loop->the_post();
 	?>
 	<?php
@@ -27,13 +28,40 @@ get_header();
 
                 <!-- Display Title and Author Name -->
                 <strong>Title: </strong><?php the_title(); ?><br />
-								<?php if(is_user_logged_in()): ?>
-										<?php
-											echo $article->visibility."<br/>";
-											if($article->isApproved())
-												echo "<strong>Approved</strong>";
-											else
-                        echo "<strong>Not Approved</strong>";
+					<?php if(is_user_logged_in()): ?>
+					<?php
+						echo $article->visibility."<br/>";
+						if($article->isApproved())
+							echo "<strong>Approved</strong><br/>";
+						else
+                        	echo "<strong>Not Approved</strong><br/>";
+
+                    	$approval = $article->getApproval($userID);
+
+                        if(!$approval && isset($_POST["uamp_approval_save"])) {
+        					$desc = $_POST["uamp_approval_desc"];
+        					$article->approve($userID, $desc);
+                            $approval = $article->getApproval($userID);
+            			}
+                        if($approval && isset($_GET["disapprove"])) {
+                            $article->disapprove($userID);
+                            $approval = $article->getApproval($userID);
+                        }  
+
+                    	if($approval) {
+                    		echo "You have approved this article.<br/>";
+                    		echo "<blockqoute>".$approval->description."</blockqoute><br/>";
+                            echo "<a href=\"?disapprove\">Disapprove</a><br/>";
+                    	}
+                    	else {
+                		?>
+                    		<form action="" method="post" />
+                    			<strong>Motivation for approval</strong><br/>
+                    			<textarea name="uamp_approval_desc"></textarea></br>
+                				<input type="submit" name="uamp_approval_save" value="Approve"/>
+                    		</form>
+                		<?php
+                    	}
 										?>
 
 								<?php endif; ?>
